091019efc91538ff5af96123607fbad665d8a81d
smcallister <stevenkmcallister@gmail.com>
2025-10-20 07:59:26 -0400

    Fixed security vulnerabilities

--- json.go
+++ json.go
@@ -29,6 +29,12 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
 	w.WriteHeader(code)
-	w.Write(dat)
 }
